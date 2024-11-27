b3424b01008feff0727447604b0391f0cdb98b65
gor1sher <164029653+gor1sher@users.noreply.github.com>
2024-11-27 18:17:06 +0300

    реализована игра

--- Main.java
+++ Main.java
@@ -1,2 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        List<String> words = new ArrayList<>();
+        words.add("house");
+        words.add("car");
+        words.add("plane");
+        words.add("animal");
+
+        Random random = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        String randomWord = words.get(random.nextInt(words.size()));
+
+        int count = randomWord.length();
+        String userWord;
+        char[] word = new char[randomWord.length()];
+        StringBuilder wholeWord = new StringBuilder();
+
+        while (true) {
+            userWord = sc.next();
+            char[] array = randomWord.toCharArray();
+            if (userWord.length() == 1) {
+                for (int i = 0; i < array.length; i++) {
+                    if (array[i] == userWord.charAt(0)) {
+                        word[i] = array[i];
+                        count--;
+                        System.out.println(array[i]);
+                    }
+                }
+                System.out.println("осталось " + count + "попыток");
+
+                wholeWord.setLength(0);
+                for (int i = 0; i < word.length; i++) {
+                    if (word[i] != '\0') {
+                        wholeWord.append(word[i]);
+                    } else {
+                        wholeWord.append("_");
+                    }
+                }
+                System.out.println("текущее угаданное слово = " + wholeWord);
+
+                if (wholeWord.toString().equals(randomWord)) {
+                    System.out.println("угадали слово: " + randomWord);
+                    break;
+                }
+            }
+        }
+    }
 }
